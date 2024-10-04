340e99d5bd5317e8ab9f9ea8147eeb36fe9ccadc
michalmeczynski <95021979+michalmeczynski@users.noreply.github.com>
2024-10-04 20:19:32 +0200

    feat: project init

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'github-search',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }]
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/github-search',
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.[jt]s?(x)', '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)']
+};
